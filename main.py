cbde50591d328d56d66c0c69e51ee43a60caa06a
neyree123 <treptaudestiny@gmail.com>
2025-07-22 22:01:50 -0500

    Created Player

--- main.py
+++ main.py
@@ -3,10 +3,20 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     
     while True:
         for event in pygame.event.get():
@@ -14,7 +24,16 @@ def main():
                 return
         
         screen.fill("black")
+
+        for object in updatable:
+            object.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
     # print("Starting Asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
